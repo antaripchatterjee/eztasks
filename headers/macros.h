85293a6a964520c5219256e43849a81e923744d8
Antarip Chatterjee <antarip.chatterjee22@gmail.com>
2024-03-08 21:16:04 +0530

    beautified macros.h

--- headers/macros.h
+++ headers/macros.h
@@ -2,10 +2,10 @@
 #define __EZTASKS_MACROS_H__
 
 
-#define EZT_NO_TIMEOUT ((tasknum_t) 0)
-#define EZT_NO_TIMEOUT_ACTION ((tasktimeoufn_t) NULL)
-#define EZTASK_OUTPUT(_T, _OUTBUFS, _TID) ((_T*) consume_task_output(_OUTBUFS, _TID, (_T[]){0}))
-#define EZT_QNIL ((taskqueue_t *)NULL)
+#define EZT_NO_TIMEOUT          ((tasknum_t) 0)
+#define EZT_NO_TIMEOUT_ACTION   ((tasktimeoufn_t) NULL)
+#define EZTASK_OUTPUT           (_T, _OUTBUFS, _TID) ((_T*) consume_task_output(_OUTBUFS, _TID, (_T[]){0}))
+#define EZT_QNIL                ((taskqueue_t *)NULL)
 
 
 #endif // __EZTASKS_MACROS_H__
